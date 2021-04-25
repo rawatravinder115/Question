c68ccc5a4951395975fec8e6581a3a6316d53088
rawatravinder115 <rawatravinder115@gmail.com>
2021-04-25 23:12:51 +0530

    bubble recursion

---
+++ sorting/bubblerec.java
@@ -0,0 +1,13 @@
+class bubblerec{
+    public static void bubble(int[] arr, int i)
+    {
+        bubble(arr,i+1);
+        if(arr[i])
+    }
+    public static void main(String[] args)
+    {
+      int[] arr={2,8,6,5,3,7,9,4};
+       bubble(arr,0);
+
+    }
+}
