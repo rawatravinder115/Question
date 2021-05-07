64e153ea31ab1d2c6cd51c51374f21215326e7c7
rawatravinder115 <rawatravinder115@gmail.com>
2021-05-07 20:28:51 +0530

    basic

---
+++ ArrayList/basic.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+class basic {
+    public static void basic(String[] args){
+        
+        ArrayList<Integer>arr=new ArrayList<Integer>();
+        // Scanner scn=new Scanner(System.in);
+        for(int i=0;i<arr.size();i++){
+            System.out.print(arr.add(i)+" ");
+        }
+
+
+
+    }
+}
