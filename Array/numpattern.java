ca7b9d23414d20a86969de719d645305b0eaf163
rawatravinder115 <rawatravinder115@gmail.com>
2021-05-08 20:13:34 +0530

    number pattern

---
+++ Array/numpattern.java
@@ -0,0 +1,22 @@
+class numpattern{
+    public static void main(String[] args){
+      int n=4;
+      int sum = 0;
+         for(int i=1;i<=4;i++){
+        for(int j=1;j<=i;j++){
+         sum=sum+1;
+         System.out.print(sum+" ");
+        }
+        System.out.println();
+}
+    }
+}
+
+
+/*
+1
+2 3
+4 5 6
+7 8 9 10
+
+*/
