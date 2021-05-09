ebab62a3347e56f31ef35ceec13409ef54084709
rawatravinder115 <rawatravinder115@gmail.com>
2021-05-09 22:00:38 +0530

    run

---
+++ Array/run.java
@@ -0,0 +1,12 @@
+class run{
+    public static void main(String[] args)
+    {
+        boolean s=false;
+        // System.out.println(s);
+        // s=!s;
+        if(s)
+        System.out.print(s+ "run");
+        else
+        System.out.print("fuck");
+    }
+}
