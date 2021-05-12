78cc3d75dcca238c505b7c238071c49448c09d66
rawatravinder115 <rawatravinder115@gmail.com>
2021-05-12 12:08:34 +0530

    graph basic

---
+++ graph/basic.java
@@ -0,0 +1,15 @@
+class basic{
+
+    public static class Edge{
+        int v =0;
+        int w= 0;
+        Edge(int v ,int w  ){
+            this.v = v;
+            this.w = w;
+        }
+    }
+
+    public static int n=7;
+    public static ArrayList<ArrayList<Edge>> graph;
+    
+}
