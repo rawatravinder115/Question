772abbd81d5aef152d74334ba3f7717bfb48796e
rawatravinder115 <rawatravinder115@gmail.com>
2021-05-13 21:01:44 +0530

    add lt1791 0ms

--- Leetcode/lt1791.java
+++ Leetcode/lt1791.java
@@ -1,3 +1,8 @@
 class lt1791{
-    
+   
+        public int findCenter(int[][] edges) {
+        int x = edges[0][0], y = edges[0][1];
+        return (y == edges[1][0] || y == edges[1][1]) ? y : x;
+    }
+
 }
