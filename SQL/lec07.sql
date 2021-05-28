e76d8b55336039e6a0d6b87806f2e7b0da32fad5
rawatravinder115 <rawatravinder115@gmail.com>
2021-05-28 23:40:41 +0530

    add lec07

---
+++ SQL/lec07.sql
@@ -0,0 +1,14 @@
+show databases;
+drop database books;
+create database books;
+show tables;
+use books;
+select * from book;
+drop table book;
+drop table my_book;
+drop table hibernate_sequence;
+select * from book_category;
+select * from book;
+select * from hibernate_sequence;
+insert into book(id,author_name,cost,name) values(3,"ton",50,"git");
+
