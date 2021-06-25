431d4e9ea13dc4f6934d470179a31b4454c89c00
rawatravinder115 <rawatravinder115@gmail.com>
2021-06-25 22:52:02 +0530

    add CountOfWays_DPMemo

--- dp/CountOfWays.java
+++ dp/CountOfWays.java
@@ -2,4 +2,19 @@ class CountOfWays{
     public static void main(String[] args) {
         
     }
+
+    // geeksforgeeks :
+    // https://www.geeksforgeeks.org/count-the-number-of-ways-to-divide-n-in-k-groups-incrementally/
+    public static int count_of_ways_DPMemo(int n, int k, int[][] dp) {
+        if (n == k || k == 1)
+            return dp[k][n] = 1;
+
+        if (dp[k][n] != 0)
+            return dp[k][n];
+
+        int NewGroup = count_of_ways_DPMemo(n - 1, k - 1, dp);
+        int ExistingGroup = count_of_ways_DPMemo(n - 1, k, dp) * k;
+
+        return dp[k][n] = NewGroup + ExistingGroup;
+    }
 }
