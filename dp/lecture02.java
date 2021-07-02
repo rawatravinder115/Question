486df32b1759e1f748c6397c260a8d0101d5286b
rawatravinder115 <rawatravinder115@gmail.com>
2021-07-02 23:44:27 +0530

    add isPalindromeSubString

--- dp/lecture02.java
+++ dp/lecture02.java
@@ -27,4 +27,22 @@ class lecture02{
             System.out.println();
         }
     }
+
+    // substring and subsequence ====================
+    public static boolean[][] isPalindromeSubstring(String str) {
+
+        int n = str.length();
+        boolean[][] dp = new boolean[n][n];
+        for (int gap = 0; gap < n; gap++) {
+            for (int i = 0, j = gap; j < n; i++, j++) {
+                if (gap == 0)
+                    dp[i][j] = true;
+                else if (gap == 1 && str.charAt(i) == str.charAt(j))
+                    dp[i][j] = true;
+                else
+                    dp[i][j] = str.charAt(i) == str.charAt(j) && dp[i + 1][j - 1];
+            }
+        }
+        return dp;
+    }
 }
