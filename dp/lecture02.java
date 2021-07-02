0ebfd0a7c4d0fd05f871e931b14a105b7991b2ef
rawatravinder115 <rawatravinder115@gmail.com>
2021-07-02 23:43:34 +0530

    add display 2d

--- dp/lecture02.java
+++ dp/lecture02.java
@@ -19,5 +19,12 @@ class lecture02{
         System.out.println();
     }
 
-   
+    public static void Display2d(int[][] dp) {
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
