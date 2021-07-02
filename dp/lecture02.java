894a6c9ba5acb45dc322f5c28ebbe3f8abcaec8a
rawatravinder115 <rawatravinder115@gmail.com>
2021-07-02 23:42:08 +0530

    add leetcode

--- dp/lecture02.java
+++ dp/lecture02.java
@@ -2,4 +2,13 @@ class lecture02{
     public static void main(String[] args) {
         
     }
+
+
+    public static void leetcode() {
+        String s = "babgbag";
+        String t = "bag";
+
+        System.out.println(numDistinct(s, t));
+
+    }
 }
