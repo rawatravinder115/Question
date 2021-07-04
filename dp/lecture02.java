6531c3388cd56332213c210964d3ca54ff82e52e
rawatravinder115 <rawatravinder115@gmail.com>
2021-07-04 22:59:39 +0530

    add count of ways dp memo

--- dp/lecture02.java
+++ dp/lecture02.java
@@ -101,4 +101,17 @@ class lecture02{
         }
         return max;
     }
+
+    public static int count_of_ways_DPMemo(int n, int k, int[][] dp) {
+        if (n == k || k == 1)
+            return dp[k][n] = 1;
+
+        if (dp[k][n] != 0)
+            return dp[k][n];
+
+        int NewGroup = count_of_ways_DPMemo(n - 1, k - 1, dp);
+        int ExistingGroup = count_of_ways_DPMemo(n - 1, k, dp) * k;
+
+        return dp[k][n] = NewGroup + ExistingGroup;
+    }
 }
