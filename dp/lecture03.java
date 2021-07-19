74d01f8f8bba5888ffb437773d98cac75d8fbdee
rawatravinder115 <rawatravinder115@gmail.com>
2021-07-19 23:23:34 +0530

    add countps part-2

--- dp/lecture03.java
+++ dp/lecture03.java
@@ -13,4 +13,12 @@ class lecture03{
 
         if (dp[i][j] != 0)
             return dp[i][j];
+            int middleString = countps(s, i + 1, j - 1, dp);
+        int excludingLast = countps(s, i, j - 1, dp);
+        int excludingFirst = countps(s, i + 1, j, dp);
+
+        int ans = excludingFirst + excludingLast;
+        return dp[i][j] = (s.charAt(i) == s.charAt(j) ? ans + 1 : ans - middleString);
+
+    }
 }
