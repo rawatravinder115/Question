fc2f070ce9e425fc774cef48b4ec5d1bd8b5172e
rawatravinder115 <rawatravinder115@gmail.com>
2021-07-21 23:16:04 +0530

    add display 2d loop

--- dp/lecture03.java
+++ dp/lecture03.java
@@ -84,7 +84,12 @@ int[][] dp = new int[a.length + 1][b.length + 1];
     }
 
     public static void Display2d(int[][] dp) {
-        
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
 
