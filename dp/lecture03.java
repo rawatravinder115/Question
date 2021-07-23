a7ef9916e119958f4aca9ea8fdd263ea8f2973ed
rawatravinder115 <rawatravinder115@gmail.com>
2021-07-24 00:18:21 +0530

    add  maxDotProduct

--- dp/lecture03.java
+++ dp/lecture03.java
@@ -221,5 +221,10 @@ int[][] dp = new int[a.length + 1][b.length + 1];
         }
         return dp[0][0];
     }
+
+    leetcode 1458 : Max Dot Product of Two Subsequences
+
+    public int maxDotProduct(int[] nums1, int[] nums2,int i, int j ,int [][] dp )
+    {
 }
 
