917611a2f3da605759470af780dffa3cd5c61812
rawatravinder115 <rawatravinder115@gmail.com>
2021-07-29 23:44:18 +0530

    add target part2

--- dp/lecture04.java
+++ dp/lecture04.java
@@ -7,4 +7,11 @@ class lecture04{
     public static void target() {
         int[] coins = { 2, 3, 5, 7 };
         int tar = 10;
+        int[][] dp = new int[coins.length + 1][tar + 1];
+        
+        // System.out.println(targetSum(coins, 0, tar, dp));
+        // System.out.println(targetSum02(coins, coins.length, tar, dp));
+
+        System.out.println(targetSum02_DP(coins, tar));
+    }
 }
