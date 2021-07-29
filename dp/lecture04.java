497e618cf0a90d98f1a067798e02b9c808bd3e15
rawatravinder115 <rawatravinder115@gmail.com>
2021-07-29 23:45:42 +0530

    add display2d system

--- dp/lecture04.java
+++ dp/lecture04.java
@@ -24,4 +24,9 @@ class lecture04{
     public static void Display2d(int[][] dp) {
         for (int i = 0; i < dp.length; i++) {
             for (int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
